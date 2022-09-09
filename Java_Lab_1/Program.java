eaf1c0250c1a650d26c99ed189f20caf2de3e340
Pazyuka Oleg <bumbox2020@gmail.com>
2022-09-09 16:03:49 +0300

    Lab 1

---
+++ Java_Lab_1/Program.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class StringCalculator {
+    public int add(String number) {
+        if (number.length() == 0)
+            return 0;
+
+        String delimiters = "";
+        String regex = "([+\\-!\\(\\){}\\[\\]^\"~*?:\\\\]|[&\\|])";
+        if (number.charAt(0) == '/') {
+            String[] part2 = number.split("\\\\n");
+            Matcher m = Pattern.compile("(?<=\\[)[^]]+").matcher(part2[0]);
+            while (m.find()) {
+                String delimiter = m.group().replaceAll(regex, "\\\\$1");
+                delimiters += delimiter+"|";
+            }
+            if (delimiters.length() == 0)
+                delimiters += number.charAt(2);
+            else
+                delimiters = delimiters.substring(0, delimiters.length() - 1);
+            number = part2[1];
+        }
+        else {
+            delimiters = ",";
+            number = number.replace("\\n", ",").replace(",,", " ,");
+        }
+
+        String[] numbers = number.split(delimiters);
+        int s = 0, n;
+        String negative = "";
+        for (String num : numbers) {
+            try {
+                n = Integer.parseInt(num);
+            }
+            catch (NumberFormatException e) {
+                System.out.print("You entered incorrect expression!");
+                return -2;
+            }
+            if (n < 0)
+                negative += n + " ";
+            else if (n < 1001)
+                s += n;
+        }
+        if (negative.length() != 0) {
+            System.out.print("Недозволені від’ємні числа: " + negative);
+            return -1;
+        }
+        else
+            return s;
+    }
+}
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringCalculator calculator = new StringCalculator();
+        String text = sc.nextLine();
+        int answer = calculator.add(text);
+        System.out.print(answer < 0 ? "":answer);
+        sc.close();
+    }
+}
