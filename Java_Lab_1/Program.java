5433e9317c8631e4d9177232e5595296bc564dc1
Pazyuka Oleg <bumbox2020@gmail.com>
2022-09-12 12:56:59 +0300

    Breakdown of the function into subfunctions, warnings

--- Java_Lab_1/Program.java
+++ Java_Lab_1/Program.java
@@ -3,33 +3,30 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class StringCalculator {
-    public int add(String number) {
-        if (number.length() == 0)
-            return 0;
+    private String regex = "([$+\\-!\\(\\){}\\[\\]^\"~*?:\\\\.]|[&\\|])";
 
-        String delimiters = "";
-        String regex = "([$+\\-!\\(\\){}\\[\\]^\"~*?:\\\\]|[&\\|])";
-        if (number.charAt(0) == '/') {
-            String[] part2 = number.split("\\\\n");
-            Matcher m = Pattern.compile("(?<=\\[)[^]]+").matcher(part2[0]);
-            while (m.find()) {
-                String delimiter = m.group().replaceAll(regex, "\\\\$1");
-                delimiters += delimiter+"|";
-            }
-            if (delimiters.length() == 0) {
-                delimiters = part2[0].substring(2, part2[0].length());
-                delimiters = delimiters.replaceAll(regex, "\\\\$1");
-            }
-            else
-                delimiters = delimiters.substring(0, delimiters.length() - 1);
-            number = part2[1];
+    class MyException extends Exception {
+        MyException(String msg) {
+            super(msg);
         }
-        else {
-            delimiters = ",";
-            number = number.replace("\\n", ",").replace(",,", " ,");
-        }
+    }
 
-        String[] numbers = number.split(delimiters);
+    private String split_delimiters(String allDelimiters, String selectedDelimiters) {
+        Matcher m = Pattern.compile("(?<=\\[)[^]]+").matcher(allDelimiters);
+        while (m.find()) {
+            String delimiter = m.group().replaceAll(regex, "\\\\$1");
+            selectedDelimiters += delimiter + "|";
+        }
+        if (selectedDelimiters.length() == 0) {
+            selectedDelimiters = allDelimiters.substring(2, allDelimiters.length());
+            selectedDelimiters = selectedDelimiters.replaceAll(regex, "\\\\$1");
+        }
+        else
+            selectedDelimiters = selectedDelimiters.substring(0, selectedDelimiters.length() - 1);
+        return selectedDelimiters;
+    }
+
+    private int summing(String[] numbers) throws MyException {
         int s = 0, n;
         String negative = "";
         for (String num : numbers) {
@@ -37,8 +34,7 @@ class StringCalculator {
                 n = Integer.parseInt(num);
             }
             catch (NumberFormatException e) {
-                System.out.print("You entered incorrect expression!");
-                return -2;
+                throw new MyException("You entered incorrect expression!");
             }
             if (n < 0)
                 negative += n + " ";
@@ -46,11 +42,30 @@ class StringCalculator {
                 s += n;
         }
         if (negative.length() != 0) {
-            System.out.print("Недозволені від’ємні числа: " + negative);
-            return -1;
+            throw new MyException("Negative numbers aren't allowed: " + negative);
         }
-        else
-            return s;
+        return s;
+    }
+
+    public int add(String number) throws MyException {
+        if (number.length() == 0)
+            return 0;
+        String delimiters = "";
+        if (number.charAt(0) == '/') {
+            String[] part2 = number.split("\\\\n");
+            if (part2.length == 1)
+                return 0;
+            number = part2[1];
+            delimiters = split_delimiters(part2[0], delimiters);
+        }
+        else {
+            delimiters = ",";
+            number = number.replace("\\n", ",").replace(",,", " ,");
+        }
+
+        String[] numbers = number.split(delimiters);
+        int sum = summing(numbers);
+        return sum;
     }
 }
 
@@ -59,7 +74,13 @@ public class Program {
         Scanner sc = new Scanner(System.in);
         StringCalculator calculator = new StringCalculator();
         String text = sc.nextLine();
-        int answer = calculator.add(text);
+        int answer = -1;
+        try {
+            answer = calculator.add(text);
+        }
+        catch(StringCalculator.MyException e) {
+            e.printStackTrace();
+        }
         System.out.print(answer < 0 ? "":answer);
         sc.close();
     }
